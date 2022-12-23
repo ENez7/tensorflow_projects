1aa22a8a1d29990d4b67e88a302b0fcdfd3bb099
Enrique Núñez <nenrique0112@gmail.com>
2022-12-22 21:00:20 -0400

    Model tunned, now 95% acc

--- mnist_image_classification.py
+++ mnist_image_classification.py
@@ -1,4 +1,5 @@
-import tensorflow as tf
+# Set de datos: MNIST
+
 from tensorflow import keras
 import matplotlib.pyplot as plt
 import numpy as np
@@ -9,22 +10,24 @@ import numpy as np
 print('Shapes')
 print(f'X Train: {x_train.shape[0]}')
 print(f'Y Train: {y_train.shape[0]}')
-print(f'X Test: {x_test.shape[0]}')
-print(f'Y Test: {y_test.shape[0]}')
+print(f'X Test : {x_test.shape[0]}')
+print(f'Y Test : {y_test.shape[0]}')
 
-n_rand = 9
-# Select n_rand random images from x_train
-images = np.random.randint(0, x_train.shape[0], n_rand)
-# Plot those n_rand random images
-for i in range(n_rand):
-    img = x_train[images[i], :, :]  # Image index, image x-axis, image y-axis
-    plt.subplot(3, 3, i + 1)  # working with 3x3 plots, i + 1 -> plot's index
-    plt.imshow(img, cmap='gray')
-    plt.axis('off')
-    plt.title(y_train[images[i]])
+# UNCOMMENT THESE LINES TO PLOT RANDOM IMAGES FROM INPUT DATA
 
-plt.suptitle(f'{n_rand} MNIST dataset images')
-plt.show()
+# n_rand = 16
+# # Select n_rand random images from x_train
+# images = np.random.randint(0, x_train.shape[0], n_rand)
+# # Plot those n_rand random images
+# for i in range(n_rand):
+#     img = x_train[images[i], :, :]  # Image index, image x-axis, image y-axis
+#     plt.subplot(4, 4, i + 1)  # working with 3x3 plots, i + 1 -> plot's index
+#     plt.imshow(img, cmap='gray')
+#     plt.axis('off')
+#     plt.title(y_train[images[i]])
+#
+# plt.suptitle(f'{n_rand} images from MNIST dataset')
+# plt.show()
 
 # Data pre-processing
 # Images must be flattened to put them into neural network
@@ -35,10 +38,10 @@ plt.show()
 x_train_flat = np.reshape(x_train, (x_train.shape[0], x_train.shape[1] * x_train.shape[2]))
 x_test_flat = np.reshape(x_test, (x_test.shape[0], x_test.shape[1] * x_test.shape[2]))
 
-# Vectors must be normalized to be between 0 and 1
+# Vectors must be normalized to fall between 0 and 1
 # 0 dark 1 light
-x_train = x_train / 255.0
-x_test = x_test / 255.0
+x_train_flat = x_train_flat / 255.0
+x_test_flat = x_test_flat / 255.0
 
 # Finally, y_train & y_test into one-hot representation
 n_classes = 10
@@ -53,11 +56,11 @@ y_test_norm = keras.utils.to_categorical(y_test, n_classes)
 np.random.seed(1)  # Restart random seed, could be omitted
 input_dim = x_train_flat.shape[1]
 output_dim = y_train_norm.shape[1]
-print(f'Input {input_dim}')
-model = keras.Sequential()
+
 neurons = 15
-model.add(keras.layers.Dense(neurons, input_dim=input_dim, activation='relu'))
-model.add(keras.layers.Dense(output_dim, activation='softmax'))
+model = keras.Sequential()
+model.add(keras.layers.Dense(neurons, input_dim=input_dim, activation='relu', name='layer_1'))
+model.add(keras.layers.Dense(output_dim, activation='softmax', name='layer_2'))
 print('Summary')
 print(model.summary())
 
@@ -69,10 +72,11 @@ model.compile(loss='categorical_crossentropy',
               optimizer=keras.optimizers.SGD(learning_rate=0.2),
               metrics=['accuracy'])
 
-n_epochs = 50
+# AS EPOCHS ARE INCREMENTED, YOU GET BETTER PRECISION
+n_epochs = 200
 b_size = 1024
-story = model.fit(x_train_flat,
-                  y_train_norm,
+story = model.fit(x=x_train_flat,
+                  y=y_train_norm,
                   epochs=n_epochs,
                   batch_size=b_size,
                   verbose=2)
@@ -87,7 +91,7 @@ plt.subplot(1, 2, 2)
 plt.plot(story.history['accuracy'])
 plt.title('Precision vs iteraciones')
 plt.ylabel('Precision')
-plt.xlabel('Itracion')
+plt.xlabel('Iteracion')
 
 plt.show()
 
@@ -97,16 +101,16 @@ print(f'Precision rate: {100 * score[1]:.1f}%')
 
 # Make some predictions
 y_pred = model.predict(x_test_flat)
-
+# Random select n_rand images from flattened images
 n_rand = 9
 images = np.random.randint(0, x_test_flat.shape[0], n_rand)
 
-for i in range(n_rand):
+for i in range(len(images)):
     idx = images[i]
     img = x_test_flat[idx, :].reshape(28, 28)
     original_cat = np.argmax(y_test_norm[idx, :])
-    predicted_cat = y_pred[idx]
-
+    predicted_cat = np.argmax(y_pred[idx])
+    # Plot the predicted results
     plt.subplot(3, 3, i + 1)
     plt.imshow(img, cmap='gray')
     plt.axis('off')
