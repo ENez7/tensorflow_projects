f355165216f0d9e06147a7acd20f10c989e417e3
enunez <nenrique0112@gmail.com>
2022-12-24 13:34:50 -0400

    Update .gitignore

---
+++ image_classification/image_classification.py
@@ -0,0 +1,15 @@
+# Image classification based on
+# https://keras.io/examples/vision/image_classification_from_scratch/
+# Dataset: https://download.microsoft.com/download/3/E/1/3E1C3F21-ECDB-4869-8368-6DEBA77B919F/kagglecatsanddogs_5340.zip
+
+# JPEG image files on disk, no leveraging pre-trained weights or
+# pre-made Keras Application model
+# Use of Keras layers for image standarization and image augmentation
+
+import tensorflow as tf
+from tensorflow import keras
+
+
+
+
+
